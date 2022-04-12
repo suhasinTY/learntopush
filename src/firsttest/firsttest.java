b1fe8ca07a925f03ba2fc425eb3c21cd758c8f5e
USER <USER@TYC1121726SUHAS>
2022-04-13 01:44:40 +0530

    where to where

---
+++ src/firsttest/firsttest.java
@@ -0,0 +1,11 @@
+package firsttest;
+
+public class firsttest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
---
+++ src/java/facto.java
@@ -0,0 +1,15 @@
+package java;
+
+public class facto {
+	public static void main(String[] args) {
+		int no=5;
+		int fact=1;
+		for(int i=no;i>=1;i--)
+		{
+			fact=fact*i;
+		}
+		System.out.println(fact);	
+			
+	}
+
+}
